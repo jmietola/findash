6b42f8672b9d035b3f3dc90ffa829d08f90bfeff
Jouni Mietola <jouni.mietola@gmail.com>
2017-09-02 18:33:37 +0300

    Create gulp task sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,11 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
