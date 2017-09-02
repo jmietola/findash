9905a3c43081e410aa6a25f1d5d668a122d4e33b
Jouni Mietola <jouni.mietola@gmail.com>
2017-09-02 18:35:06 +0300

    Add gulp task to move javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,10 @@ gulp.task('sass', function() {
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
+
+// Move the javascript files into our /src/js folder
+gulp.task('js', function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
+        .pipe(gulp.dest("src/js"))
+        .pipe(browserSync.stream());
+});
